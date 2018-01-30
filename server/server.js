7433b94461dbfb4f6ed1273c90e72423e49a33c1
Suman Kadiyala <skckadiyala@gmail.com>
2018-01-29 17:25:35 -0700

    Test Post todo

--- server/server.js
+++ server/server.js
@@ -34,6 +34,7 @@ app.listen(3000, () => {
 
 
 
+module.exports = {app};
 
 
 //
---
+++ server/tests/server.test.js
@@ -0,0 +1,55 @@
+const expect = require('expect');
+const request = require('supertest');
+
+const {app} = require('./../server');
+const {Todo} = require('./../models/todo');
+
+
+beforeEach((done) => {
+    Todo.remove({}).then(()=> {
+     done();
+    })
+})
+
+describe ('POST /todo', () => {
+    it('should create a new todo', (done) => {
+        var text = 'Test the Todo';
+
+        request(app)
+            .post('/todo')
+            .send({text})
+            .expect(200)
+            .expect((res)=> {
+                expect(res.body.text).toBe(text);
+            })
+            .end((err,res) => {
+            if (err){
+                return done(err);
+            }
+
+            Todo.find().then((todo)=>{
+                expect(todo.length).toBe(1);
+                expect(todo[0].text).toBe(text);
+                done();
+            }).catch((e) => done(e));
+            });
+    });
+    it('should not create a new todo', (done) => {
+
+        request(app)
+            .post('/todo')
+            .send({})
+            .expect(400)
+            .end((err,res) => {
+                if (err){
+                    return done(err);
+                }
+
+                Todo.find().then((todo)=>{
+                    expect(todo.length).toBe(0);
+                    done();
+                }).catch((e) => done(e));
+            });
+
+    });
+});
